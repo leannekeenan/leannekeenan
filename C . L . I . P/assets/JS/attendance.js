5cfa14a9f09da676a8692b62e745841cd5b6289c
Leanne Keenan <l_keenan@outlook.com>
2023-10-31 19:28:20 -0700

    working on attendance table

--- C . L . I . P/assets/JS/attendance.js
+++ C . L . I . P/assets/JS/attendance.js
@@ -14,37 +14,34 @@ let form = document.querySelector('form');
 form.addEventListener('submit', function(event) {
   event.preventDefault();
   let shift = document.querySelector('input[name="shift"]:checked').value;
-  let timepunch = document.querySelector('input[name="timepunch"]:checked').value;
-  let lunch = document.querySelector('input[name="lunch"]:checked').value;
-  
-  let firstName = document.getElementById('firstName').value;
-  let lastName = document.getElementById('lastName').value;
+  let timepunches = document.querySelectorAll('input[name^="timepunch"]:checked');
+  let timepunchIn = document.querySelector('#clockInOption');
+  let timepunchOut = document.querySelector('#clockOutOption');
+  let lunchStart = document.querySelector('input[name="lunchStart"]:checked').value;
+  let lunchEnd = document.querySelector('input[name="lunchEnd"]:checked').value;
   let employeeID = document.getElementById('employeeID').value;
   let todaysDate = document.getElementById('todaysDate').value;
- 
   let campus = document.getElementById('campuses').value;
 
   let currentTime = updateClock(); // Call the updateClock function and store the current time
-
-  let historyRow = document.getElementById('history');
-  historyRow.innerHTML = ''; // Clear the row
-
-  let cells = ['dateHistory', 'idHistory', 'campusHistory', 'shiftHistory', 'clockInHistory', 'clockOutHistory'];
-  cells.forEach(cell => {
-    let newCell = document.createElement('td');
-    newCell.id = cell;
-    historyRow.appendChild(newCell);
-  });
 
   document.getElementById('dateHistory').innerText = todaysDate;
   document.getElementById('idHistory').innerText = employeeID;
   document.getElementById('campusHistory').innerText = campus;
   document.getElementById('shiftHistory').innerText = shift;
 
-  if (timepunch === 'In') {
+  if (timepunchIn) {
     document.getElementById('clockInHistory').innerText = currentTime; // Display the current time in the 'clockInHistory' cell
-  } else if (timepunch === 'Out') {
+  } 
+  if (timepunchOut) {
     document.getElementById('clockOutHistory').innerText = currentTime; // Display the current time in the 'clockOutHistory' cell
+  }
+
+  if(lunchStart) {
+    document.getElementById('clockInHistory').innerText = currentTime;
+  }
+  if(lunchEnd) {
+    document.getElementById('clockOutHistory').innerText = currentTime;
   }
 });
 
