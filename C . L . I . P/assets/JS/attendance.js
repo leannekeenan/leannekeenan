406aaaf8834f0dddcefc7bf365aab4a277512c35
Leanne Keenan <l_keenan@outlook.com>
2023-11-09 23:28:08 -0800

    Fixed attendance.js data entries

--- C . L . I . P/assets/JS/attendance.js
+++ C . L . I . P/assets/JS/attendance.js
@@ -13,77 +13,77 @@ document.addEventListener("DOMContentLoaded", function() {
 
   let form = document.querySelector('form');
   form.addEventListener('submit', function(event) {
-      event.preventDefault();
-      let employeeID = document.getElementById('employeeID').value;
-      let shift = document.querySelector('input[name="shift"]:checked').value;
-      let todaysDate = document.getElementById('todaysDate').value;
-      let campus = document.getElementById('campuses').value;
+    event.preventDefault();
+    let employeeID = document.getElementById('employeeID').value;
+    let shift = document.querySelector('input[name="shift"]:checked').value;
+    let todaysDate = document.getElementById('todaysDate').value;
+    let campus = document.getElementById('campuses').value;
+ 
+    let start = document.querySelector('input[id="start"]:checked'); 
+    let stop = document.querySelector('input[id="stop"]:checked'); 
+     
+    let currentTime = updateClock(); // Call the updateClock function and store the current time
+ 
+    // Get the table
+    let table = document.querySelector('.AnalyticsTable');
+ 
+    // Create a unique identifier for the row
+    let rowIdentifier = todaysDate + shift + employeeID + campus;
+ 
+    // Check if a row for the current shift already exists
+    let row = Array.from(table.rows).find(row => row.getAttribute('data-identifier') === rowIdentifier);
+ 
+    if (!row) {
+        // If not, create a new row
+        row = document.createElement('tr');
+        row.setAttribute('data-identifier', rowIdentifier);
 
-      let start = document.querySelector('input[id="start"]:checked'); 
-      let stop = document.querySelector('input[id="stop"]:checked'); 
-      
-      let currentTime = updateClock(); // Call the updateClock function and store the current time
+        // Create new table data elements and set their text content to the form values
+        let dateCell = document.createElement('td');
+        dateCell.textContent = todaysDate;
 
-      // Get the table
-      let table = document.querySelector('.AnalyticsTable');
+        let idCell = document.createElement('td');
+        idCell.textContent = employeeID;
 
-      // Create new table data elements and set their text content to the form values
-      let dateCell = document.createElement('td');
-      dateCell.textContent = todaysDate;
+        let campusCell = document.createElement('td');
+        campusCell.textContent = campus;
 
-      let idCell = document.createElement('td');
-      idCell.textContent = employeeID;
+        let shiftCell = document.createElement('td');
+        shiftCell.textContent = shift;
 
-      let campusCell = document.createElement('td');
-      campusCell.textContent = campus;
-
-      let shiftCell = document.createElement('td');
-      shiftCell.textContent = shift;
-
-      let clockInCell = document.createElement('td');
-      if (start) {
-        clockInCell.textContent = currentTime;
-      }
-
-      let clockOutCell = document.createElement('td');
-      if (stop) {
-        clockOutCell.textContent = currentTime;
-      }
-
-      // Create a new table row
-      let row = document.createElement('tr');
-
-      // Append these new table data elements to the new table row
-      row.appendChild(dateCell);
-      row.appendChild(idCell);
-      row.appendChild(campusCell);
-      row.appendChild(shiftCell);
-      row.appendChild(clockInCell);
-      row.appendChild(clockOutCell);
-
-      let rows = table.querySelectorAll('tr');
-      let existingRow = null;
-      for (let i = 0; i < rows.length; i++) {
-        let cells = rows[i].querySelectorAll('td');
-        if (cells[0].textContent === todaysDate && cells[3].textContent === shift) {
-          existingRow = rows[i];
-          break;
-        }
-      }
-
-      if (existingRow) {
-        let startCell = existingRow.querySelector('td:nth-child(5)');
-        let stopCell = existingRow.querySelector('td:nth-child(6)');
+        let clockInCell = document.createElement('td');
         if (start) {
-          startCell.textContent += ', ' + currentTime;
+          clockInCell.textContent = currentTime;
         }
-        if (stop) {
-          stopCell.textContent += ', ' + currentTime;
-        }
-      } else {
-        table.appendChild(row);
-      }
-  });
 
+        let clockOutCell = document.createElement('td');
+        if (stop) {
+          clockOutCell.textContent = currentTime;
+        }
+
+        // And append these new table data elements to the new row
+        row.appendChild(dateCell);
+        row.appendChild(idCell);
+        row.appendChild(campusCell);
+        row.appendChild(shiftCell);
+        row.appendChild(clockInCell);
+        row.appendChild(clockOutCell);
+
+        // And append the new row to the table
+        table.appendChild(row);
+    } else {
+        // Check if the row already has an end time
+        if (row.cells[5].textContent) {
+            // If it does, prevent the form submission and alert the user
+            event.preventDefault();
+            alert('Please clock out before you can clock in again.');
+        } else {
+            // Update the start and stop times on the row
+            row.cells[4].textContent = start ? currentTime : row.cells[4].textContent;
+            row.cells[5].textContent = stop ? currentTime : row.cells[5].textContent;
+        }
+    }
+  });
+ 
   setInterval(updateClock, 1000);
 });
