74ab1b481fd69b8b8bbc14009861cceaa8ae5f69
Leanne Keenan <l_keenan@outlook.com>
2024-03-18 13:36:24 -0700

    Added page elements to script.js

---
+++ Dungeon Crawl Card Game/assets/js/script.js
@@ -0,0 +1,18 @@
+let gameDeck =document.getElementById('game-deck');
+let forge =document.getElementById('forge');
+let scout =document.getElementById('scout');
+let investigate =document.getElementById('investigate');
+let consider =document.getElementById('consider');
+let boost =document.getElementById('boost');
+let gameRiver =document.getElementById('game-river');
+let card1 =document.getElementById('card1');
+let card2 =document.getElementById('card2');
+let card3 =document.getElementById('card3');
+let discard =document.getElementById('discard');
+
+
+gameDeck.addEventListener('click', playCard)
+
+function playCard() {
+    
+}
