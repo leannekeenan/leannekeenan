1cbb5c194c8942b86f5082864a73b1afde5cd13d
Leanne Keenan <l_keenan@outlook.com>
2024-03-25 18:02:59 -0700

    Modified Card Images/worked on randomizer

--- Dungeon Crawl Card Game/assets/js/script.js
+++ Dungeon Crawl Card Game/assets/js/script.js
@@ -24,7 +24,40 @@ let underground = document.getElementById('underground');
 let kingdomEntry = document.getElementById('kingdomEntry');
 
 
-let menu = document.getElementById('menu')
+let backgroundMenu = document.getElementById('backgroundMenu');
+
+let deck = document.getElementById('deck');
+
+let rightPath = '/Dungeon Crawl Card Game/assets/images/cards/Right Path.png';
+let deadEnd = '/Dungeon Crawl Card Game/assets/images/cards/Dead End.png';
+let trap = '/Dungeon Crawl Card Game/assets/images/cards/Trap.png';
+let monster = '/Dungeon Crawl Card Game/assets/images/cards/Monster.png';
+let treasure = '/Dungeon Crawl Card Game/assets/images/cards/Treasure.png';
+
+let cards = [rightPath, deadEnd, trap, monster, treasure];
+let card;
+
+deck.addEventListener('click', randomize)
+
+function randomize() {
+   let randomizer = Math.floor(Math.random() * cards.length);
+   let randomCard = cards[randomizer];
+   console.log(randomCard)
+
+   let newCard = document.createElement('img');
+   newCard.src = randomCard;
+
+   if(card1.innerHTML === '') {
+    card1.appendChild(newCard)
+   }
+   else if(card1.innerHTML !== '' && card2.innerHTML === '') {
+    card2.appendChild(newCard)
+   }
+   else if(card1.innerHTML !== '' && card2.innerHTML !== '' && card3.innerHTML === '') {
+    card3.appendChild(newCard)
+   }
+}
+randomize()
 
 
 forest.addEventListener('click', forestBackground)
@@ -36,7 +69,7 @@ swamp.addEventListener('click', swampBackground)
 underground.addEventListener('click', undergroundBackground)
 kingdomEntry.addEventListener('click', kingdomEntryBackground)
 
-menu.addEventListener('change', select)
+backgroundMenu.addEventListener('change', select)
 
 function forestBackground() {
 document.body.style.backgroundImage = "url('/Dungeon Crawl Card Game/assets/images/dungeons/forest at sunset.jpg')";
@@ -71,7 +104,7 @@ function kingdomEntryBackground() {
 }
 
 function select() {
-    let option = menu.value;
+    let option = backgroundMenu.value;
 
     switch(option) {
         case 'forest': forestBackground();
@@ -94,6 +127,8 @@ function select() {
 
 select()
 
+
+
 /*
 *---What do I want to have happen---*
 
@@ -104,12 +139,16 @@ select()
     monster
     treasure
 
-+ Build a deck using the five card value types. It should contain:
+[
+    + Build a deck of 55 cards using the five card value types. It should contain:
     15 right path cards
     15 dead end cards
     10 traps
     10 monsters 
     5 treasures
+]
+
++ Begin the game with an empty river
 
 + Allow the deck to randomly select one of the 55 cards in the deck as values for the river cards
 
@@ -120,9 +159,6 @@ select()
 + When a player clicks the deck a third time, a card is placed face down in object card3 within the river object.
 
 + If the player clicks the deck when the river is full, an alert informs the player they must clear a space in the river before asking for another card
-
-
-+ Once a card is revealed within the river, it remains in place until all cards in the river are revealed
 
 + Once the river is full, player will have the option to select an action/effect from the table to apply to their turn
 
@@ -142,15 +178,24 @@ select()
         - shuffle the deck
         - place deck on top of card just placed at the bottom of the deck
 
-    + When a player clicks the consider action, the player can reveal two cards in the virer instead of just one
+    + When a player clicks the "consider" action, the player can reveal two cards in the virer instead of just one
         - Of the two showing, the player can choose one
         - once choosen, the deck needs to be clicked to add a new card to the deck
 
-    + When a player clicks the morale action, if the river contains a dead end card, it is removed and replaced with a new card from the deck
+    + When a player clicks the "morale" action, if the river contains a dead end card, it is removed and replaced with a new card from the deck
 
 + If no effects are choosen, a player can still choose a card from the river to reveal
 
++ When a player clicks a face down card in the river, the card is fliped to reveal its card type
+
++ Once the card has its type revealed, when clicked again, the card is placed in the "discard" deck
+
++ Once a card is revealed within the river, it remains in place until all cards in the river are revealed
+
++ if a "Right Path" card is revealed in teh river, all cards in the river get placed in teh discard pile and three more cards need to be pulled from the deck
+
 + If all three river cards are all the same card, reshuffle the deck and start again
 
+Once all 55 cards have been played, the player can click the empty deck to reshuffle the cards and present a new deck, keeping any current cards in play 
 
 */
