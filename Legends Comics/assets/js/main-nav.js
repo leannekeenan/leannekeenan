43e6f17b46bfe34c5c612b2de5994e78483424a9
Leanne Keenan <l_keenan@outlook.com>
2024-05-01 15:43:21 -0700

    Fixed sticky nav bar

--- Legends Comics/assets/js/main-nav.js
+++ Legends Comics/assets/js/main-nav.js
@@ -1,33 +1,12 @@
 window.addEventListener('scroll', function() {
-    var nav = document.querySelector('.main-nav');
-    var section = document.getElementById('home');
-    var sectionHeight = section.clientHeight;
+  console.log('Scroll event triggered!');
+  var navbar = document.getElementById('main-nav');
+  var navContainer = document.getElementById('nav-container');
+  var containerTop = navContainer.offsetTop;
 
-    if (window.pageYOffset > sectionHeight) {
-        nav.classList.add('sticky-nav');
-    } else {
-        nav.classList.remove('sticky-nav');
-    }
-});
-
-
-window.onscroll = function() {myFunction()};
-
-// Get the navbar
-var navbar = document.getElementById("main-nav");
-
-// Get the offset position of the navbar
-var sticky = navbar.offsetTop;
-
-// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
-function myFunction() {
-  if (window.pageYOffset >= sticky) {
-    navbar.classList.add("sticky")
+  if (window.pageYOffset > containerTop) {
+    navbar.classList.add('sticky-nav');
   } else {
-    navbar.classList.remove("sticky");
+    navbar.classList.remove('sticky-nav');
   }
-}
-
-
-
-
+});
