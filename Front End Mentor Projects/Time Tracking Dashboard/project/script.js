43ee6863107b5c25fea13e6ac93d5728097a648c
Leanne Keenan <l_keenan@outlook.com>
2024-05-26 16:22:56 -0700

    Working on FEtch Request for "Time Tracking Dashboard" Project

---
+++ Front End Mentor Projects/Time Tracking Dashboard/project/script.js
@@ -0,0 +1,24 @@
+
+//.user .stat-options a 
+const daily = document.getElementById('daily');
+const weekly = document.getElementById('weekly');
+const monthly = document.getElementById('monthly');
+
+//dashboard values (current and last week (LW))
+const workHours = document.getElementById('work-hours');
+const workHoursLW = document.getElementById('work-hours-lw');
+const playHours = document.getElementById('play-hours');
+const playHoursLW = document.getElementById('play-hours-lw');
+const studyHours = document.getElementById('study-hours');
+const studyHoursLW = document.getElementById('study-hours-lw');
+const exerciseHours = document.getElementById('exercise-hours');
+const exerciseHoursLW = document.getElementById('exercise-hours-lw');
+const socialHours = document.getElementById('social-hours');
+const socialHoursLW = document.getElementById('social-hours-lw');
+const personalHours = document.getElementById('personal-hours');
+const personalHoursLW = document.getElementById('personal-hours-lw');
+
+const timeValue = document.querySelectorAll('time-value');
+const previousTimeValue = document.querySelectorAll('previous-time-value')
+
+
