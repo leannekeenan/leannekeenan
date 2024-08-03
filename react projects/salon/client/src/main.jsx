f74b3db5ff7c8133b2ced31cef9af5799b372159
Leanne Keenan <l_keenan@outlook.com>
2024-08-02 18:02:33 -0700

    Created individual pages - need to add routing

--- react projects/salon/client/src/main.jsx
+++ react projects/salon/client/src/main.jsx
@@ -3,6 +3,16 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+import About from './pages/About.jsx'
+import Appointments from './pages/Appointments.jsx'
+import Bookings from './pages/Bookings.jsx'
+import Home from './pages/Home.jsx'
+import Login from './pages/Home.jsx'
+import Services from './pages/Services.jsx'
+import Signin from './pages/Signin.jsx'
+import Staff from './pages/Staff.jsx'
+
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
