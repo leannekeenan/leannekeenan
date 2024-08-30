1f67b187b2141c352926b2173f9bd8c2ce5f3a98
Leanne Keenan <l_keenan@outlook.com>
2024-08-30 12:20:22 -0700

    Adding Router

--- react projects/booking/client/src/main.jsx
+++ react projects/booking/client/src/main.jsx
@@ -1,10 +1,26 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+
+
+import Admin from './pages/Admin.jsx'
+import Booking from './pages/Booking.jsx'
+import Home from './pages/Home.jsx'
+import Services from './pages/Services.jsx'
+import Staff from './pages/Staff.jsx'
+
+
 import App from './App.jsx'
 import './index.css'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <Router>
+      <Routes>
+          <Route/>
+          <Route/>
+          <Route/>
+          <Route/>
+          <Route/>
+      </Routes>
+  </Router>
 )
